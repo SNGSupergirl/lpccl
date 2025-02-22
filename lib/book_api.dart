b280d9727b70f1f4e1b4d95646e12dc2c8efbb39
SNGSupergirl <SNGSupergirl@gmail.com>
2025-02-22 00:53:01 -0800

    Recognizes Admins now and will take them to "admin panel" when you click on the option in settings

--- lib/book_api.dart
+++ lib/book_api.dart
@@ -11,25 +11,25 @@ Future<Map<String, dynamic>?> fetchBookData(String isbn) async {
   if (response.statusCode == 200) {
     final Map<String, dynamic> jsonData = jsonDecode(response.body);
     if (jsonData['totalItems'] > 0) {
-      final item = jsonData['items']; // Access the first item in the list
+      final item = jsonData['items'][0]; // Access the first item in the list
       final volumeInfo = item['volumeInfo'];
       return {
-      'title': volumeInfo['title'],
-      'authors': volumeInfo['authors'], //
-  'publisher': volumeInfo['publisher'],
-  'publishedDate': volumeInfo['publishedDate'],
-  'description': volumeInfo['description'],
-  'pageCount': volumeInfo['pageCount'],
-  'categories': volumeInfo['categories'], //
-  'averageRating': volumeInfo['averageRating'],
-  'ratingsCount': volumeInfo['ratingsCount'],
-  'imageLinks': volumeInfo['imageLinks']?? {}, //
-  'isbn': isbn,
-  };
+        'title': volumeInfo['title'],
+        'authors': volumeInfo['authors']?.cast<String>() ?? [],
+        'publisher': volumeInfo['publisher'],
+        'publishedDate': volumeInfo['publishedDate'],
+        'description': volumeInfo['description'],
+        'pageCount': volumeInfo['pageCount'],
+        'categories': volumeInfo['categories']?.cast<String>() ?? [],
+        'averageRating': volumeInfo['averageRating'],
+        'ratingsCount': volumeInfo['ratingsCount'],
+        'imageLinks': volumeInfo['imageLinks'] ?? {},
+        'isbn': isbn,
+      };
+    } else {
+      return null; // Book not found
+    }
   } else {
-  return null; // Book not found
-  }
-  } else {
-  throw Exception('Failed to fetch book data');
+    throw Exception('Failed to fetch book data');
   }
 }
